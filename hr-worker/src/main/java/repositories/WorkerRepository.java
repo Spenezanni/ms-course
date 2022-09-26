909a56034c96d05c65fc9be124d1c1a3d63ef5ba
Spenezanni <gustavospenezanni@gmail.com>
2022-09-26 18:01:06 -0300

    Implementação de classes

---
+++ hr-worker/src/main/java/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
