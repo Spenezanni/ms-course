86176d7d59609d85a2f56a5972238bc9c0da570d
Spenezanni <gustavospenezanni@gmail.com>
2022-09-28 18:30:55 -0300

    Hystrix Implementation

--- hr-payroll/src/main/java/com/project/payroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/project/payroll/controller/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.project.payroll.entities.Payment;
 import com.project.payroll.service.PaymentService;
 
@@ -17,11 +18,17 @@ public class PaymentController {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = paymentService.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
 	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", 200.0, 10);
+		return ResponseEntity.ok(payment);
+	
+	}
 
 }
--- hr-worker/src/main/java/com/project/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/project/hrworker/HrWorkerApplication.java
@@ -3,7 +3,9 @@ package com.project.hrworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableAutoConfiguration
 public class HrWorkerApplication {
