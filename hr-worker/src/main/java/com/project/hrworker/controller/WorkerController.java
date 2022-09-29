49b3ec4d96749ef8b61b0ba99a42709ea7a7b09d
Spenezanni <gustavospenezanni@gmail.com>
2022-09-29 17:54:28 -0300

    Implementation of Config-Server

--- hr-worker/src/main/java/com/project/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/project/hrworker/controller/WorkerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,10 @@ public class WorkerController {
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
+	
 	@Autowired
 	private Environment env;
 	
@@ -37,14 +42,22 @@ public class WorkerController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findWorkerById(@PathVariable Long id) {
 		
-	try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+//	try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker worker = workerService.findWorkerById(id);
 		return new ResponseEntity<>(worker, HttpStatus.OK);
+	
+	}
+	
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig );
+		return ResponseEntity.noContent().build();
 	
 	}
 	
